9d3693981c7772db96caa35ccc1b4be9138a1560
roxnlopez <roxnlopez@gmail.com>
2017-07-13 15:08:57 -0600

    finsih line

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function Player(racer, gameboard) {
 	};
 
 	this.move = function() {
-		$(this.racer).css("margin-left", "+=10%");
+		$(this.racer).css("start", "+=10%");
 	};
 }
 
@@ -26,4 +26,28 @@ function Play() {
 		player2.prepare();
 		setListener();
 	};
+
+	//create the listener and apply keys
+	var setListener = function() {
+		$(document).keyup(function(event) {
+			if (event.which == 38 && event.which == 37 && event.which == 39) {
+				player1.move();
+			} else if (event.which == 90 && event.which == 88 && event.which == 67) {
+				player2.move();
+			}
+			//winner declared based on reaching finish line
+			if (parseInt(player1.racer).css("finish") >= parseInt(gameboard).css("height")) {
+					gameOver(player1);
+		      } else if (parseInt(player2.racer).css("finish") >= parseInt(gameboard).css("height")) {
+		        	gameOver(player2);
+		      }
+		});   
+	};
 }
+
+var Home = function(winner) {
+	if (confirm("Game Over!" + winner + " wins! Play again.")) {
+		player1.css("finish", "0%");
+		player2.css("finish", "0%");
+	}
+};
