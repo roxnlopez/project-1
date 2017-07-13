1ff0406701b2333e76640efbe5516e061ffcb5b2
roxnlopez <roxnlopez@gmail.com>
2017-07-13 12:36:48 -0600

    adding players

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 console.log("up and running!");
 
-document.querySelector("r1");
-document.querySelector("r2");
-//console.log("r1");
-//console.log("r2");
+function Player(name){
+	this.name = name;
+}
+
+
+function Game() {
+	var field = document.querySelector(".gameboard");
+	var player1 = new Player(document.querySelector("r1"), "10%");
+	var player2 = new Player(document.querySelector("r2"), "10%");
+	console.log(field);
+	console.log(player1);
+	console.log(player2);
+
+	this.start = function() {
+		player1.prepare();
+		player2.prepare();
+		setListener();
+	};
+}
+Game();
+
 
 function startGame() {
 	alert("GO!");
