1140a68547324903b9d311ddcc4ebde9f34970ef
roxnlopez <roxnlopez@gmail.com>
2017-07-13 11:45:48 -0600

    setimeout for start

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ document.querySelector("r1");
 document.querySelector("r2");
 //console.log("r1");
 //console.log("r2");
+
+function startGame() {
+	alert("GO!");
+}
+var myTimer = setTimeout(startGame, 3000);
