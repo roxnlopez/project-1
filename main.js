45d9b28e8ba6883088f6c22503bfa7525b01769a
roxnlopez <roxnlopez@gmail.com>
2017-07-22 15:33:21 -0600

    added some test js

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
     console.log( "ready!" );
-//get start button to start game
 //players and gameboard
 function Player(racer, gameboard) {
 	this.racer = racer;
 	this.gameboard = gameboard;
 	this.timerVar = setInterval(countTimer, 1000);
 	var totalSeconds = 0;
-
 	this.prepare = function() {
 		$(this.racer).css(".gameboard", this.gameboard);
 	};
-
 	this.move = function() {
 		let actualNumber = parseInt($(this.racer).css("top"));
 		actualNumber -= 10;
@@ -18,7 +15,6 @@ function Player(racer, gameboard) {
 	};
 }
  	console.log(Player);
-//set up a game function with the players and the field
 	console.log('hi');
 //function Play() {
 //	var field = $('.gameboard')[0];
@@ -30,54 +26,42 @@ function Player(racer, gameboard) {
 //		player2.prepare();
 //		setListener();
 //	};
-		function clock() {
-			$('body').prepend('#.game');
-			var totalSeconds = 0;
-			setInterval(setTime, 1000);
-			function setTime() {
-				++totalSeconds;
-				$('#clock > seconds').html(pad(totalSeconds%60));
-			}
-		function pad(val) {
-			var valString = val + "";
-				if(valString.length < 2) {
-					return "0" + valString;
-				} else {
-					return valString;
-				}
-		}
-		}
-//everytime i move player, current values, need to know gameboard dimensions 
-	//create the listener and apply keys
-	//var setListener = function() {
+		
 	//set keys so players can move around field
 		$(document).keydown(function(event) {
-			//38 is up
 			if (event.which == 38) {
-				$('#r1').stop(true).animate({"top": "-=40px"});
-			//37 is left	
+				$('#r1').stop(true).animate({"top": "-=80px"});
 			} else if (event.which == 37) {
-				$('#r1').stop(true).animate({"left": "-=40px"});
-			//39 is right	
+				$('#r1').stop(true).animate({"left": "-=80px"});
 			} else if (event.which == 39) {
-				$('#r1').stop(true).animate({"left": "+=40px"});
+				$('#r1').stop(true).animate({"left": "+=80px"});
 			} 
 
-			//z is 90 is left
 			if (event.which == 90 ) {
-				$('#r2').stop(true).animate({left: "-=40"});
-				//$('#r2').animate({marginLeft: "-=10"});
-			// 88 is x is up	
+				$('#r2').stop(true).animate({left: "-=80"});
 			} else if (event.which == 88){
-				$('#r2').stop(true).animate({top: "-=40"});
-			//67 is c is right	
+				$('#r2').stop(true).animate({top: "-=80"});
 			} else if (event.which == 67){
-				$('#r2').stop(true).animate({left: "+=40"});
+				$('#r2').stop(true).animate({left: "+=80"});
 			}
-	
-			//winner declared based on reaching finish line
-			//try less than
-			//remember top, left, right(left + wdith of gameboard)
+	$(function () {
+	var game = new Game();
+	var $moves = $('.game-moves');
+
+	$('body').on('click', '.new-round', function () {
+		game.newRound();
+	});
+
+	game.on('move', function (e) {
+		$moves.text(e.moves.length);
+	});
+
+	game.on('newRound gameOver', function () {
+		$moves.empty();
+	});
+
+	game.newRound();
+});
 				
 		console.log(parseInt($('#r1').css("top")));
 		console.log(parseInt($('#finish').css("top")));
@@ -92,19 +76,18 @@ function Player(racer, gameboard) {
 			      // 	gameOver(player2);
 			      //}
 		      }
-		    
-		      	//if event.which = up then win
-				//if(event.which === 38 || event.which === 88 < parseInt($('#finish').css("top"))) {
-		//});   
-//}
-	
-//var gameOver = function(winner) {
-	//if(current score > highScore)  {  highScore = current score;  }
-   //if(person presses space)  {  initialize();  }
 
-//	if (confirm("Game Over!" + winner + " wins! Play again.")) {
-//		player1.css("finish", "0%");
-//		player2.css("finish", "0%");
-//	}
+		    //game timer   
+		var timerVar = setInterval(countTimer, 1000);
+	    var totalSeconds = 0;
+	    function countTimer() {
+	      ++totalSeconds;
+	      var hour = Math.floor(totalSeconds /3600);
+	      var minute = Math.floor((totalSeconds - hour*3600)/60);
+	      var seconds = totalSeconds - (hour*3600 + minute*60);
+	  
+	     document.getElementById("timer").innerHTML = "Timer: " + seconds;
+    }
+	
 });
 	console.log("Roxann");
