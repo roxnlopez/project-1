3d3cde1b7897444e2b475b0b4a94076a0369115d
roxnlopez <roxnlopez@gmail.com>
2017-07-23 08:40:59 -0600

    edited readme

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 For my project, I am thinking of something similar to the racer game. But maybe, one player against the computer. Haven't decided on that bit yet. And I want to put in random landmines throughout the course. I've also thought about scoreboards, timers, working reset button, and a player button that shows life left or some aspect like that.
 Trello: https://trello.com/b/g3q2QUdr/project-1
 <img src="Wireframe.jpg">
-
+<a href="file:///Users/roxannnlopez/Documents/Fundamentals/project-1/index.html"/>
 -think about difficulty level or just one level of ease(?) OR OR OOORRR different amounts of time. 10sec 30sec 45 sec 
 and maybe different sizes of grids
--Game: start, stop, timer, reset
--Board: draw board out, plantMines throughout, calculateDistance, traverse, reveal, gameOver
--Field: element, x, y, flagged, revealed, mine, empty, 
--both players go at same time/both want to reach finish/no need for timer bc based on distance (if oodles of time, make timer for funsies)
--bombs randomized
+-Game: timer, reset
+-Board: draw board out, gameOver
+-Field: element, x, y, flagged, revealed
+-both players go at same time/both want to reach finish
 -forcing both players to move forward with JS (setInterval(stuffToDo, seconds b/n))
--ok, i just fixed the alignment of my page. I'm starting on my characters...those little fuckers better fit.
+-ok, i just fixed the alignment of my page. I'm starting on my characters...chose pacman to save time on sprites.
 -added plain css
 -got my characters in
 -set timeout for start of game(setInterval kept re-starting and wouldn't shut up)
 -aiming to assign keys to players now.
 JS is stupid....or my head won't cooperate...jquery instead.
--fixed the players to move this morning and now they're on strike those little fuckers. Looks pretty good though.
+-fixed the players to move this morning.  Looks pretty good though.
 -scrapped the mines. let's just move the pinky toe....get the players to stay on their damn field.
--added media queries
+-added media queries...need to add to html as well.
 -finally got the players to get to finish line and win. Now just to get the page to only alert for up button.
+-Got an alert for a winner working.
+-Changed up the css design of page to be more consistent and more aesthetically pleasing.
+-remember to add in a link to the game page at the top of the readme.
+-looking into multiple rounds for game.
+-working on getting my timer to work...keeps re-setting...or getting the setInternval to bring up a timer. weird stuff going on.
--- main.js
+++ main.js
@@ -1,93 +1,134 @@
-    console.log( "ready!" );
+console.log( "ready!" );
 //players and gameboard
-function Player(racer, gameboard) {
-	this.racer = racer;
+function Game(playerOne, playerTwo) { //set beginning variables
+	setInterval(moveRacer, 10);
+	this.moveRacer = moveRacer;
+	this.playerOne = new Player(playerOne);
+	this.playerTwo = new Player(playerTwo);
 	this.gameboard = gameboard;
-	this.timerVar = setInterval(countTimer, 1000);
-	var totalSeconds = 0;
-	this.prepare = function() {
-		$(this.racer).css(".gameboard", this.gameboard);
-	};
-	this.move = function() {
-		let actualNumber = parseInt($(this.racer).css("top"));
-		actualNumber -= 10;
-		$(this.racer).css("top", actualNumber);
-	};
+	this.roundScore = 0;
+	this.activePlayer = this.playerOne;
+	var game = newGame();
+	var restart_btn = restart_btn;
 }
- 	console.log(Player);
-	console.log('hi');
-//function Play() {
-//	var field = $('.gameboard')[0];
-//	var player1 = new Player('#r1', "10%");
-//	var player2 = new Player('#r2', "10%");
-	//set listener
-//	this.start = function() {
-//		player1.prepare();
-//		player2.prepare();
-//		setListener();
-//	};
-		
-	//set keys so players can move around field
-		$(document).keydown(function(event) {
-			if (event.which == 38) {
-				$('#r1').stop(true).animate({"top": "-=80px"});
-			} else if (event.which == 37) {
-				$('#r1').stop(true).animate({"left": "-=80px"});
-			} else if (event.which == 39) {
-				$('#r1').stop(true).animate({"left": "+=80px"});
-			} 
 
-			if (event.which == 90 ) {
-				$('#r2').stop(true).animate({left: "-=80"});
-			} else if (event.which == 88){
-				$('#r2').stop(true).animate({top: "-=80"});
-			} else if (event.which == 67){
-				$('#r2').stop(true).animate({left: "+=80"});
+function Player(playerName, score) { 
+	this.playerName = playerName;
+	this.score = 0;
+}
+
+Game.prototype.endRound = function() { //set end of round function
+	if(this.activePlayer === this.playerOne) {
+		this.playerOne.score += this.roundScore;
+		if(this.playerOne.score >= 100) {
+			return playerOne.playerName + " wins the race!";
+		} else {
+			this.activePlayer = this.playerTwo;
+		} else {
+			this.playerTwo.score += this.roundScore;
+			if(this.playerTwo.score >= 100) {
+				return playerTwo.playerName + " wins the race!";
+			} else {
+				this.activePlayer = this.playerOne;
 			}
-	$(function () {
-	var game = new Game();
-	var $moves = $('.game-moves');
+		}
+		this.roundScore = 0;
+	}
+};
 
-	$('body').on('click', '.new-round', function () {
-		game.newRound();
-	});
+// Game.prototype.playRound = function() { //build play function
+// 	var
+// }
 
-	game.on('move', function (e) {
-		$moves.text(e.moves.length);
-	});
+$function() {
+	$("form#player-form").submit(function(event) {
+		event.preventDefault();
+		var playerOneName = $("input#player-one-name").val();
+		var playerTwoName = $("input#player-two-name").val();
+		var newGame = new Game(playerOneName, playerTwoName);
 
-	game.on('newRound gameOver', function () {
-		$moves.empty();
-	});
+		//start a new game
+		$('#racer1').fadeOut(750);
+		$('#new-game').fadeOut(750);
+		$('#live-game').delay(750).fadeIn(1000);
+		$('#player-one-name').val(" ");
+		$('#player-two-name').val(" ");
+		$('#live-game h2').text(newGame.activePlayer.playerName + "'s turn.");
 
-	game.newRound();
-});
-				
-		console.log(parseInt($('#r1').css("top")));
-		console.log(parseInt($('#finish').css("top")));
-				if (parseInt($('#r1').css("top")) < parseInt($('#finish').css("top"))) {
-					alert("Player1 wins the race!");
-			//now my page reloads		
-					window.location.reload(true); 
-				//		gameOver(player1);
-			      } else if (parseInt($('#r2').css("top")) < parseInt($('#finish').css("top"))) {
-			      	alert("Player2 wins the race!");
-			      	window.location.reload(true); 
-			      // 	gameOver(player2);
-			      //}
-		      }
+		//player one setup
+	})
+}
+// $('.start').click(function () {
+// 	var counter = 10;
+// 	var interval = setInterval(function() {
+// 		counter--;
+// 		if (counter >= 0) {
+// 			span = $('#timer');
+// 			span.html(counter);
+// 			$('.start').attr('disabled', 'disabled');
+// 		}
+// 		if (counter === 0) {
+// 			clearInterval(interval);
+// 			return $('.gameboard').toggleClass("gameboard"), 
+//                    $("#win").css('display', 'block'), 
+//                    $(".start").css('display', 'none'), 
+//                    $("#reload").css('display', 'block'),
+//                    $("startGame").removeAttr("disabled", "disabled"),
+//                    $("#tally").append(score + "!"),
+//                     resetScore2();
+//             }
+//     }, 1000);
+// });
+// console.log(setInterval);
+console.log(Player);
+	//set keys so players can move around field
+	$(document).keydown(function(event) {
+		if (event.which == 38) {
+			$('#r1').stop(true).animate({"top": "-=80px"});
+		} else if (event.which == 37) {
+			$('#r1').stop(true).animate({"left": "-=80px"});
+		} else if (event.which == 39) {
+			$('#r1').stop(true).animate({"left": "+=80px"});
+		} 
+		if (event.which == 90 ) {
+			$('#r2').stop(true).animate({left: "-=80"});
+		} else if (event.which == 88){
+			$('#r2').stop(true).animate({top: "-=80"});
+		} else if (event.which == 67){
+			$('#r2').stop(true).animate({left: "+=80"});
+		}
 
-		    //game timer   
-		var timerVar = setInterval(countTimer, 1000);
-	    var totalSeconds = 0;
-	    function countTimer() {
-	      ++totalSeconds;
-	      var hour = Math.floor(totalSeconds /3600);
-	      var minute = Math.floor((totalSeconds - hour*3600)/60);
-	      var seconds = totalSeconds - (hour*3600 + minute*60);
-	  
-	     document.getElementById("timer").innerHTML = "Timer: " + seconds;
-    }
-	
+	console.log(parseInt($('#r1').css("top")));
+	console.log(parseInt($('#finish').css("top")));
+		if (parseInt($('#r1').css("top")) < parseInt($('#finish').css("top"))) {
+			alert("Player1 wins the race!");		
+			location.reload();
+			window.location.reload(false);
+	//gameOver(player1);
+	    } else if (parseInt($('#r2').css("top")) < parseInt($('#finish').css("top"))) {
+	      	alert("Player2 wins the race!");
+	      	location.reload();
+			window.location.reload(false);
+	//gameOver(player2)
+		}
+
+	// //set game rounds
+	// if ('.winner' == parseInt($('#finish').css("top"))) {
+	// 	location.reload();
+	// } else {
+	// 	console.log("Try again.");
+	// }
+
+	// //game timer to run 60 seconds  
+	// var timerVar = setInterval(countTimer, 1000);
+	//    var totalSeconds = 0;
+ //    function countTimer() {
+ //      ++totalSeconds;
+ //      var hour = Math.floor(totalSeconds /3600);
+ //      var minute = Math.floor((totalSeconds - hour*3600)/60);
+ //      var seconds = totalSeconds - (hour*3600 + minute*60);	  
+	//   document.getElementById("timer").innerHTML = "Timer: " + seconds;
+ //    }
+
 });
 	console.log("Roxann");
