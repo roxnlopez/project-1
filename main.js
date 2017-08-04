acb5a23404c9fde307d5667dd6565cb9d443a893
roxnlopez <roxnlopez@gmail.com>
2017-08-03 19:54:53 -0600

    css html js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function beginRound() {
         //changing innerHTML to timer
         document.getElementById("demo").innerHTML = timer;
     	//if timer equals 0, clear Interval
-        if (timer === 0) {
+        if (timer <= 0) {
         	clearInterval(myint);
 		}	
     }, 1000);
@@ -48,36 +48,48 @@ function beginRound() {
 			$('#r1').stop(true).animate({"left": "+=80px"});
 		} 
 		if (event.which == 90 ) {
-			$('#r2').stop(true).animate({left: "-=80"});
+			$('#r2').stop(true).animate({"left": "-=80px"});
 		} else if (event.which == 88){
-			$('#r2').stop(true).animate({top: "-=80"});
+			$('#r2').stop(true).animate({"top": "-=80px"});
 		} else if (event.which == 67){
-			$('#r2').stop(true).animate({left: "+=80"});
+			$('#r2').stop(true).animate({"left": "+=80px"});
 		}
-		if (parseInt($('#r1').css("top")) < parseInt($('#finish').css("top"))) {
-			console.log();
-			document.removeEventListener("keydown", keydownFunction);
-			object.style.counterRest = "";
-			// alert("Player1 wins the race!");
+		if (parseInt($('#r1').css("top")) <= 55) {
 			//end timer
+			document.removeEventListener("keydown", keydownFunction);
+			$('#r1').css("top", "80%");
+			$('#r2').stop(true);
+			$('#r1').css("left", "35%");
+			$('#r2').css("top", "80%");
+			$('#r1').stop(true);
+			$('#r2').css("left", "60%");
 	   		//need a var to keep track of how many times player won game
 	   		score1++;
    			document.getElementById("score1").innerHTML = score1;
    			//create function to signify round has ended
    			round++;
-   			myFunction();
-		} else if (parseInt($('#r2').css("top")) < parseInt($('#finish').css("top"))) {
+   			//if round is 4 game over...see who has more points (score 1 and score 2)
+   			clearInterval(myint);
+   			// myFunction();
+		} 
+		else if (parseInt($('#r2').css("top")) <= 55) {
+			//end timer
 			document.getElementById("finish").removeEventListener("keydown", keydownFunction);
-			object.style.counterRest = "";
-	   		// alert("Player2 wins the race!");
-	   		//end timer
+			$('#r1').css("top", "80%");
+			$('#r2').stop(true);
+			$('#r1').css("left", "35%");
+			$('#r2').stop(true);
+			$('#r2').css("top", "80%");
+			$('#r2').css("left", "60%");
 	   		//need a var to keep track of how many times player won game
 	   		score2++;
 	   		document.getElementById("score2").innerHTML = score2;
 	   		//create function to signify round has ended
 	   		round++;
-	   		myFunction();
+	   		clearInterval(myint);
+	   		// myFunction();
 		}
 	}
+
 	document.addEventListener("keydown", keydownFunction);
 }
