2fb6c630cd53a75cd5fb11f0040ec77721ccb361
roxnlopez <roxnlopez@gmail.com>
2017-08-04 12:29:38 -0600

    tabs

--- main.js
+++ main.js
@@ -1,54 +1,132 @@
 console.log( "ready!" );
-//players and gameboard
-function Game(playerOne, playerTwo) { //set beginning variables
-	setInterval();
-	this.moveRacer = moveRacer;
-	this.gameboard = gameboard;
-	this.roundScore = 0;
-	var game = newGame();
+//players scores
+var score1 = 0;
+var score2 = 0;
+var round = 1;
+var timer = 0;
+var myint;
+var roundOver = false;
+var gameInterval;
+
+function displayWinMessage() {
+	if(score1 > score2) {
+		alert("Player 1 wins!");
+	} else {
+		alert("Player 2 wins!");
+	}	
+	round = 1;
+	score1 = 0;
+	score2 = 0;
+	timer = 0;
+	roundOver = false;
+	clearInterval(myint);
+	clearInterval(gameInterval);
 }
 
-function Player(playerName, score) { 
-	this.playerName = playerName;
-	this.score = 0;
+//set countdown to initialize when page loads with message displayed
+function myFunction() {
+	setTimeout(function() {
+		newFunction("On Your Marks!");
+	}, 1000); 
+	setTimeout(function() {
+		newFunction("Get Set!");
+	}, 2000);
+	setTimeout(function() {
+		newFunction("GO!");
+		beginRound();
+	}, 3000);
 }
 
-console.log(Game);
-console.log("Roxann");
-//set keys so players can move around field
-$(document).keydown(function(event) {
-	if (event.which == 38) {
-		$('#r1').stop(true).animate({"top": "-=80px"});
-	} else if (event.which == 37) {
-		$('#r1').stop(true).animate({"left": "-=80px"});
-	} else if (event.which == 39) {
-		$('#r1').stop(true).animate({"left": "+=80px"});
-	} 
-	if (event.which == 90 ) {
-		$('#r2').stop(true).animate({left: "-=80"});
-	} else if (event.which == 88){
-		$('#r2').stop(true).animate({top: "-=80"});
-	} else if (event.which == 67){
-		$('#r2').stop(true).animate({left: "+=80"});
-	}
-	if (parseInt($('#r1').css("top")) < parseInt($('#finish').css("top"))) {
-		alert("Player1 wins the race!");		
-//gameOver(player2);
-  } else if (parseInt($('#r2').css("top")) < parseInt($('#finish').css("top"))) {
-   	alert("Player2 wins the race!");
+function newFunction(input) {
+	document.getElementById("demo").innerHTML = input;
+}
+
+function beginRound() {
+	document.getElementById("round").innerHTML = round;
+	roundOver = false;
+	 	clearInterval(myint);
+	//creat timer
+    myint = setInterval(function(){ 
+        //timer going down by one
+    	timer++;
+        //changing innerHTML to timer
+        document.getElementById("demo").innerHTML = timer;	
+    }, 1000);
+    gameInterval = setInterval(function() {
+    	checkForWin();
+    }, 50);
+
+
+    //players start
+    //set keys so players can move around field
+    function keydownFunction(event){
+    	if (roundOver) return;
+		if (event.which == 38) {
+			$('#r1').stop(true).animate({"top": "-=80px"});
+		} else if (event.which == 37) {
+			$('#r1').stop(true).animate({"left": "-=80px"});
+		} else if (event.which == 39) {
+			$('#r1').stop(true).animate({"left": "+=80px"});
+		} 
+		if (event.which == 90 ) {
+			$('#r2').stop(true).animate({"left": "-=80px"});
+		} else if (event.which == 88){
+			$('#r2').stop(true).animate({"top": "-=80px"});
+		} else if (event.which == 67){
+			$('#r2').stop(true).animate({"left": "+=80px"});
+		}
 	}
 
-//game timer to run 60 seconds  
-var sec = 0;
-function pad ( val ) { return val > 3 ? val : "0" + val; }
-setInterval(function(){
-    $(".seconds").html(pad(++sec%60));
-    $("#minutes").html(pad(parseInt(sec/60,10)));
-}, 1000);
-var timer = setInterval ( function(){
-	console.log("next round");
-}, 1000 );
-clearInterval ( timer );
+	function checkForWin(){
+		if (roundOver && round === 4) {
+			clearInterval(gameInterval);
+			displayWinMessage();
+		} else {
+			if (parseInt($('#r1').css("top")) <= 55) {
+			if (roundOver) return;
+			//end timer
+			$('#r1').css("top", "80%");
+			$('#r1').stop(true);
+			$('#r1').css("left", "35%");
+			$('#r2').css("top", "80%");
+			$('#r2').stop(true);
+			$('#r2').css("left", "60%");
+			console.log("hello");
+	   		//need a var to keep track of how many times player won game
+	   		score1++;
+	   		var round1timer = timer;
+	   		timer = 1;
+   			document.getElementById("score1").innerHTML = "<span>Player 1 Score: " + score1 + " " + "<span>Timer: (" + round1timer + ")</span>";
+   			//create function to signify round has ended
+   			round++;
+   			console.log(timer);
+   			//if round is 4 game over...see who has more points (score 1 and score 2)
+   			clearInterval(myint);
+   			//myFunction();
+   			roundOver = true;
+		} 
+		else if (parseInt($('#r2').css("top")) <= 55) {
+			if (roundOver) return;
+			//end timer
+			$('#r1').css("top", "80%");
+			$('#r1').stop(true);
+			$('#r1').css("left", "35%");
+			$('#r2').css("top", "80%");
+			$('#r2').stop(true);
+			$('#r2').css("left", "60%");
+	   		//need a var to keep track of how many times player won game
+	   		score2++;
+	   		var round1timer = timer;
+	   		timer = 1;
+   			document.getElementById("score2").innerHTML = "<span> Player 2 Score: " + score2 + " " + "<span>Timer: (" + round1timer + ")</span>";
+	   		//create function to signify round has ended
+	   		round++;
+	   		clearInterval(myint);
+	   		//myFunction();
+	   		roundOver = true;
+			}
+		}
+	}
 
-	console.log(timer);
-});
+	document.addEventListener("keydown", keydownFunction);
+}
